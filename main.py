a4f8d9e1c97b100dde9176e7373f034d4620e687
Marko Brzak <marko.brzak.5@gmail.com>
2023-01-02 19:05:29 +0100

    Implemented renaming and distributing pages functionalities, finished all stages four of repack.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageTk
 from tkinter import ttk
 import shutil
 import time
+import numpy
 
 global bufferPath
 bufferPath = 'images/buffer.jpg'
@@ -51,7 +52,12 @@ def selectFolder():
 
 def repack():
 
+
     startTime = time.time()
+
+    
+    global repackButton
+    repackButton.config(state=DISABLED)
 
     global mangaChaptersPath
 
@@ -144,6 +150,137 @@ def repack():
 
 
 
+    startTime = time.time()
+
+    numberOfChapters = len(next(os.walk(mangaChaptersPath))[1])
+        
+    # Makes folder for all files to be put into
+    os.mkdir(mangaChaptersPath + mangaName + " - All Chapters")
+
+    dirPath = mangaChaptersPath + mangaName + " - All Chapters/"
+
+
+    i = 0
+
+    for chapterNum in numpy.arange(1, numberOfChapters+1, 1):
+        print(chapterNum)
+
+        # addZero is used for cases where number of files in one chapter exceeds 100
+        addZero = ""
+        numberforRename = 10
+
+        chapter = mangaChaptersPath + "Chapter " + str(chapterNum) + "/"
+
+        # folder path
+        dir_path = chapter
+        fileCount = 0
+
+        # Get number of files
+        # Iterate directory
+        for path1 in os.listdir(dir_path):
+            # check if current path is a file
+            if os.path.isfile(os.path.join(dir_path, path1)):
+                fileCount += 1
+        print('File Count:', fileCount)
+
+
+        if(fileCount >= 100):
+            addZero = "0"
+            numberforRename = 100
+
+        
+        for k in range(fileCount, numberforRename-1, -1):
+            if(os.path.isfile(chapter + str(k) + ".jpg")):
+                os.rename(chapter + str(k) + ".jpg", dirPath + str(i+k) + ".jpg")
+            elif(os.path.isfile(chapter + str(k) + ".png")):
+                os.rename(chapter + str(k) + ".png", dirPath + str(i+k) + ".jpg")
+            elif(os.path.isfile(chapter + str(k) + ".webp")):
+                os.rename(chapter + str(k) + ".webp", dirPath + str(i+k) + ".jpg")
+
+        i = i + 1
+
+        for l in range(1, numberforRename):
+
+            if(l>=10):
+                addZero = ""
+            if(os.path.isfile(chapter + "0" + addZero + str(l) + ".jpg")):
+                os.rename(chapter + "0" + addZero + str(l) + ".jpg", dirPath + str(i) + ".jpg")
+            elif(os.path.isfile(chapter + "0" + addZero + str(l) + ".png")):
+                os.rename(chapter + "0" + addZero + str(l) + ".png", dirPath + str(i) + ".jpg")
+            elif(os.path.isfile(chapter + "0" + addZero + str(l) + ".webp")):
+                os.rename(chapter + "0" + addZero + str(l) + ".webp", dirPath + str(i) + ".jpg")
+            i = i + 1
+
+        if(fileCount >= 100):
+            i = i + fileCount - 100
+        else:
+            i = i + fileCount - 10
+
+        os.rmdir(chapter)
+
+    renamePages.config(text='Renaming pages ✓', font=boldFont)
+    
+    renamePagesT = (time.time() - startTime)
+
+    renamePagesT = str(round(renamePagesT, 2)) + 's'
+
+    renamePagesTime.config(text=renamePagesT)
+
+    startTime = time.time()
+
+
+    all_chapters = mangaChaptersPath + mangaName + " - All Chapters/"
+
+    number_of_files = len(os.listdir(all_chapters))
+
+    start = -999
+    end = 0
+
+
+    while(number_of_files > 0):
+        
+        start = start + 1000
+
+        if(number_of_files - 1000 <= 0):
+            end = end + number_of_files
+            os.mkdir(mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end))
+            print("Folder created: ", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end))
+            for k in range(start, end+1):
+                if(os.path.isfile(all_chapters + str(k) + ".jpg")):
+                    os.rename(all_chapters + str(k) + ".jpg", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end) + "\\" + str(k) + ".jpg")
+                elif(os.path.isfile(all_chapters + str(k) + ".png")):
+                    os.rename(all_chapters + str(k) + ".png", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end) + "\\" + str(k) + ".png")
+                elif(os.path.isfile(all_chapters + str(k) + ".webp")):
+                    os.rename(all_chapters + str(k) + ".webp", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end) + "\\" + str(k) + ".webp")
+
+            break
+
+        end = end + 1000
+
+        os.mkdir(mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end))
+
+        for k in range(start, end+1):
+            if(os.path.isfile(all_chapters + str(k) + ".jpg")):
+                os.rename(all_chapters + str(k) + ".jpg", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end) + "\\" + str(k) + ".jpg")
+            if(os.path.isfile(all_chapters + str(k) + ".png")):
+                os.rename(all_chapters + str(k) + ".png", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end) + "\\" + str(k) + ".png")
+            if(os.path.isfile(all_chapters + str(k) + ".webp")):
+                os.rename(all_chapters + str(k) + ".webp", mangaChaptersPath + mangaName + " " + str(start) + "-" + str(end) + "\\" + str(k) + ".webp")
+
+
+        number_of_files = number_of_files - 1000
+
+    os.rmdir(all_chapters)
+
+    distributePages.config(text='Distributing pages ✓', font=boldFont)
+    
+    distributePagesT = (time.time() - startTime)
+
+    distributePagesT = str(round(distributePagesT, 2)) + 's'
+
+    distributePagesTime.config(text=distributePagesT)
+
+
 def main():
 
     eraseRoot()
@@ -200,9 +337,17 @@ def main():
     renamePages = Label(stagesFrame, text='Renaming pages ...', font=boldFont)
     renamePages.place(x=5, y=45)
 
+    global renamePagesTime
+    renamePagesTime = Label(stagesFrame, text='', font=boldFont, justify=LEFT)
+    renamePagesTime.place(x=550, y=45)
+
     global distributePages
     distributePages = Label(stagesFrame, text='Distributing pages ...', font=boldFont)
     distributePages.place(x=5, y=65)
+
+    global distributePagesTime
+    distributePagesTime = Label(stagesFrame, text='', font=boldFont, justify=LEFT)
+    distributePagesTime.place(x=550, y=65)
 
 root = Tk()
 root.geometry(centerScreen(700, 525))
